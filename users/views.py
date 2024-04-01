c06f2aa06296af362f3a291412a0a6253816f57a
scr6w <playerzone06@gmail.com>
2024-03-31 21:00:57 -0300

    config statics and add home route + dirs struture

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.views import View
+from .views_handler.home.home_handler import home_handler
+import re
+
+class Home(View):
+    def get(self, request):
+        return home_handler(request)
+        
